2f550b544ac2726b70c4841cf60954e154f2789b
gggaryteo <garyteoprograms@gmail.com>
2023-01-14 18:19:11 +0800

    styled navigation bar and added Navlink

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,37 @@
-function Navbar() {
+// styles and images
+import "./Navbar.css";
+import ProjectLogo from "../assets/Foongrum.png";
 
+
+// import React Packages
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { FaHome, FaSignInAlt, FaUserPlus } from "react-icons/fa";
+
+
+
+function Navbar() {
   return (
-      <div className="container">
-        Navbar
-      </div>
+    <div className="navbar">
+      <nav className="links">
+
+        <ul>
+          <li className="logo">
+            <img src={ProjectLogo} alt="project-logo" />
+          </li>
+          <li>
+            <NavLink to="/"> <FaHome/> Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login"> <FaSignInAlt/> Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register"> <FaUserPlus/> Signup</NavLink>
+          </li>
+        </ul>
+
+      </nav>
+    </div>
   );
 }
 export default Navbar;
