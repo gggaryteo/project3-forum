40e5c219f6abf4e4ecd04a772f314d9f9ffd89e1
gggaryteo <garyteoprograms@gmail.com>
2023-01-15 18:33:12 +0800

    changed min height

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,7 +24,7 @@ export default function StickyFooter() {
       sx={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "50vh",
+        minHeight: "100vh",
       }}
     >
       <CssBaseline />
